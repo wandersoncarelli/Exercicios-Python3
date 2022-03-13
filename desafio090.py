8491059e80981e55e62742212104c804b21e9a5f
Wanderson <wanderson.carelli@gmail.com>
2022-03-13 01:21:37 -0300

    Resolução de alguns exercícios.

--- desafio090.py
+++ desafio090.py
@@ -2,3 +2,15 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o
 # conteúdo da estrutura na tela.
 # ======================================================================================================================
+aluno = dict()
+aluno['Nome'] = input('Nome: ')
+aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+if aluno['Média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+elif 5 <= aluno['Média'] < 7:
+    aluno['Situação'] = 'Em recuperação'
+else:
+    aluno['Situação'] = 'Reprovado'
+print()
+for k, v in aluno.items():
+    print(f'{k} é igual a {v}.')
--- desafio091.py
+++ desafio091.py
@@ -2,3 +2,29 @@
 # Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
 # dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 # ======================================================================================================================
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogadores = dict()
+for i in range(0, 4):
+    jogadores[input(f'Jogador {i + 1}: ')] = randint(1, 6)
+
+print()
+for k, v in jogadores.items():
+    print(f'{k} tirou {v}...')
+    sleep(1.5)
+
+print()
+ranking = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+posicao = 1
+
+print(10 * '=', 'RANKING', 10 * '=')
+for i in range(0, 4):
+    if i == 0:
+        print(f'{posicao}º - {ranking[i][0]} com {ranking[i][1]} pontos.')
+    elif ranking[i][1] == ranking[i - 1][1]:
+        print(f'     {ranking[i][0]} com {ranking[i][1]} pontos.')
+    else:
+        posicao += 1
+        print(f'{posicao}º - {ranking[i][0]} com {ranking[i][1]} pontos.')
